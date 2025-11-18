620dfe83303d591cb71d70be76f0ff749369bf6d
UstyUsty <alexsysalov07@gmail.com>
2025-11-18 22:16:55 +0700

    first commit

--- script.js
+++ script.js
@@ -16,7 +16,7 @@ function showUserInfo() {
         <h3>Информация о пользователе:</h3>
         <p>ID: ${user?.id || 'Неизвестно'}</p>
         <p>Имя: ${user?.first_name || 'Неизвестно'}</p>
-        <p>Фамилия: ${user?.last_name || ''}</p>
+        <p>Сходить нахуй: ${user?.last_name || ''}</p>
         <p>Username: @${user?.username || 'Неизвестно'}</p>
     `;
 }
